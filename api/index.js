23c8be74c6afc50b4c5e0a86a09bce100fe2dde3
“syd” <zheng.yuxi@outlook.com>
2024-02-06 17:59:30 -0500

    create signup and encryption

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.routes.js';
+import authRoutes from './routes/auth.routes.js';
 
 dotenv.config();
 
@@ -18,9 +19,12 @@ mongoose
 
 const app = express();
 
+// can receive expression in json
+app.use(express.json());
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 })
 
 app.use('/api/user', userRoutes);
-
+app.use('/api/auth', authRoutes);
